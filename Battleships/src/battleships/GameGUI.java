d9b8bc8de09589ef199570cdacb44df4d679820a
Schoersch <Schoersch@10.3.97.17>
2016-05-23 09:49:02 +0200

    Added first Game Gui

--- Battleships/src/battleships/GameGUI.java
+++ Battleships/src/battleships/GameGUI.java
@@ -5,13 +5,19 @@
  */
 package battleships;
 
+import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.EventQueue;
+import java.awt.GridLayout;
 import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JSeparator;
 import javax.swing.WindowConstants;
 
 /**
@@ -20,31 +26,51 @@ import javax.swing.WindowConstants;
  */
 public class GameGUI extends JFrame{
     
-    private final JMenuBar menuBar = new JMenuBar();
-    private final JMenu menuFile = new JMenu("File");
-    private final JMenuItem mNew = new JMenuItem("New");
-    private final JMenuItem mExit = new JMenuItem("Exit");
+    //private final JMenuBar menuBar = new JMenuBar();
+    //private final JMenu menuFile = new JMenu("File");
+    //private final JMenuItem mNew = new JMenuItem("New Game");
+    //private final JMenuItem mExit = new JMenuItem("Exit");
+    private JLabel lStatus = new JLabel("Status");
 
     public GameGUI() throws HeadlessException {
         
         super("Battleships");
         
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setSize(500,500);
+        setSize(1000,500);
         setResizable(false);
         
-        menuBar.add(menuFile);
-        menuFile.add(mNew);
-        menuFile.addSeparator();
-        menuFile.add(mExit);
+        //menuBar.add(menuFile);
+        //menuFile.add(mNew);
+        //menuFile.addSeparator();
+        //menuFile.add(mExit);
         
-        setJMenuBar(menuBar);
+        GridLayout GUILayout = new GridLayout(0,3);
+        
+        final JPanel player = new JPanel();
+        player.setLayout(GUILayout);
+        JPanel pane = new JPanel();
+        pane.setLayout(new GridLayout(2,3));
+        
+        for(int i=1;i<=36;i++){
+            add(new Button(""+i));
+        }
+        
+        add(lStatus);
+        
+        //setJMenuBar(menuBar);
+        
+        pane.add(player, BorderLayout.NORTH);
+        pane.add(new JSeparator(), BorderLayout.CENTER);
+        pane.add(player, BorderLayout.SOUTH);
+        
         setVisible(true);
         
         //
-        mExit.addActionListener((ActionEvent e) -> {
+        /*mExit.addActionListener((ActionEvent e) -> {
             System.exit(0);
-        });
+        });*/
+        
         
     }
     
