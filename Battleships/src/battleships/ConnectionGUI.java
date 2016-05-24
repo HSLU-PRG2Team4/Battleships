69d5b93b8d9878980c22641b1d822fd9b869df14
Rafael <Rafael@10.3.111.222>
2016-05-24 13:26:22 +0200

    Merge origin/master into rafael
    Conflicts:
    	Battleships/src/battleships/GameControl.java

--- Battleships/src/battleships/ConnectionGUI.java
+++ Battleships/src/battleships/ConnectionGUI.java
@@ -6,6 +6,7 @@
 package battleships;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
@@ -16,12 +17,11 @@ import javax.swing.*;
  */
 public class ConnectionGUI extends JDialog implements ActionListener {
 
-    JButton buttonServer;
-    JButton buttonClient;
-    JLabel connectionLabel;
-    JPanel panelTop;
-    JPanel panelBottom;
-
+    private JButton buttonServer;
+    private JButton buttonClient;
+    private JLabel connectionLabel;
+    private JPanel panelTop;
+    private JPanel panelBottom;
     private GameView gameView;
 
     public ConnectionGUI(GameView gameView) {
@@ -30,30 +30,23 @@ public class ConnectionGUI extends JDialog implements ActionListener {
     }
 
     private void initUI() {
-        this.setTitle("Choose Connection");        
+        this.setTitle("Battleship - Choose Connection");        
         this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
         this.setModal(true);
-        this.setSize(550, 150);
+        this.setPreferredSize(new Dimension(400,120));
         panelTop = new JPanel();
         panelBottom = new JPanel();
-
-        // Leeres JLabel-Objekt wird erzeugt
         connectionLabel = new JLabel("Please choose your connection");
-
-        //Drei Buttons werden erstellt
         buttonServer = new JButton("Server");
         buttonClient = new JButton("Client");
 
-        //Buttons werden dem Listener zugeordnet
         buttonServer.addActionListener(this);
         buttonClient.addActionListener(this);
 
-        //Buttons werden dem JPanel hinzugefügt
         panelTop.add(buttonServer);
         panelTop.add(buttonClient);
-
-        //JLabel wird dem Panel hinzugefügt
         panelBottom.add(connectionLabel);
+        
         this.add(panelTop, BorderLayout.NORTH);
         this.add(panelBottom, BorderLayout.SOUTH);
         
