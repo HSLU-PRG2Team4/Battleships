22f416aabd57b28a3802998c4b37c8fe61d84776
Rafael <Rafael@10.3.111.222>
2016-05-24 11:11:11 +0200

    Merge origin/master into rafael
    Conflicts:
    	Battleships/src/battleships/GameControl.java

--- Battleships/src/battleships/GameHost.java
+++ Battleships/src/battleships/GameHost.java
@@ -6,6 +6,8 @@
 package battleships;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.logging.Level;
@@ -20,25 +22,77 @@ public class GameHost {
 
     private Socket clientSocket;
     private ServerSocket serverSocket;
-   
-    public void waiting(){       
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
+    private GameControl gameControl;
+
+    public GameHost(GameControl gameControl) {
+        this.gameControl = gameControl;
+    }
+
+    public void waiting() {
         try {
-             serverSocket = new ServerSocket(4444);
-             clientSocket = serverSocket.accept();
-             JOptionPane.showMessageDialog(null, "Verbindung zu Client hergestellt. Game wird gestartet.");
-        } catch (IOException e) {             
-             throw new RuntimeException(
-                     "Error accepting client connection", e);
-        }           
-    }    
-    
-    public void connect(String ip){       
+            serverSocket = new ServerSocket(4444);
+            clientSocket = serverSocket.accept();
+            out = new ObjectOutputStream(clientSocket.getOutputStream());
+             in = new ObjectInputStream(clientSocket.getInputStream());
+            JOptionPane.showMessageDialog(null, "Verbindung zu Client hergestellt. Game wird gestartet.");
+        } catch (IOException e) {
+            throw new RuntimeException(
+                    "Error accepting client connection", e);
+        }
+    }
+
+    public void connect(String ip) {
         try {
             clientSocket = new Socket(ip, 4444);
-             JOptionPane.showMessageDialog(null, "Verbindung zu Server hergestellt. Game wird gestartet.");
-        } catch (IOException e) {             
-             throw new RuntimeException(
-                     "Error accepting client connection", e);
-        }           
+             out = new ObjectOutputStream(clientSocket.getOutputStream());
+             in = new ObjectInputStream(clientSocket.getInputStream());
+            JOptionPane.showMessageDialog(null, "Verbindung zu Server hergestellt. Game wird gestartet.");
+        } catch (IOException e) {
+            throw new RuntimeException(
+                    "Error accepting client connection", e);
+        }
     }
+
+    public void sendShot(String coordinates) {
+        try {
+            out.writeObject(coordinates);
+        } catch (IOException ex) {
+            Logger.getLogger(GameHost.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void sendGrid(Grid grid) {
+        try {
+            out.writeObject(grid);
+        } catch (IOException ex) {
+            Logger.getLogger(GameHost.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public String receiveShot() {
+        String coordinates = "";
+        try {
+            in = new ObjectInputStream(clientSocket.getInputStream());
+            coordinates = (String) in.readObject();
+        } catch (IOException | ClassNotFoundException ex) {
+            Logger.getLogger(GameHost.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return coordinates;
+    }
+
+    public Grid receiveGrid() {
+        Grid opponentGrid = null;
+        try {
+            in = new ObjectInputStream(clientSocket.getInputStream());
+            opponentGrid = (Grid) in.readObject();
+        } catch (IOException | ClassNotFoundException ex) {
+            Logger.getLogger(GameHost.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return opponentGrid;
+    }
+
 }
