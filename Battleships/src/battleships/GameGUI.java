79ec200a37120c517a36a6c378dd70aa4b92c975
Schoersch <Schoersch@10.3.97.17>
2016-05-24 13:48:55 +0200

    deleted unused imports

--- Battleships/src/battleships/GameGUI.java
+++ Battleships/src/battleships/GameGUI.java
@@ -6,22 +6,15 @@
 package battleships;
 
 import java.awt.BorderLayout;
-import java.awt.Button;
 import java.awt.Color;
-import java.awt.EventQueue;
 import java.awt.GridLayout;
 import java.awt.HeadlessException;
-import java.awt.Label;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.JSeparator;
 import javax.swing.WindowConstants;
 
 /**
